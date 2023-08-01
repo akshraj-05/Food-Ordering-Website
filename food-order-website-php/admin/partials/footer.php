7addb056a76942da0cff0f28d083c77a61882880
akshraj chavda <90257725+akshraj-05@users.noreply.github.com>
2023-08-01 09:37:48 +0530

    initial commit

---
+++ food-order-website-php/admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2023 All rights reserved,  Developed By - <a href="https://www.linkedin.com/in/akshraj-chavda-242441208/"> Student's Of LDCE</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
---
+++ food-order-website-php/partials-front/footer.php
@@ -0,0 +1,14 @@
+<!-- social Section Starts Here -->
+
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved. Designed By <a href="https://www.linkedin.com/in/akshraj-chavda-242441208/">Student's Of LDCE</a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
